2f641503ef08e1408b67b0ddaf6bb491077bc56a
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-04-01 16:21:47 +0700

    allowed cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,22 @@ import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+
+  // Enable CORS for development - allows requests from web and mobile
+  app.enableCors({
+    origin: [
+      'http://localhost:8081',
+      'http://localhost:3000',
+      'http://10.0.189.45:8081',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
